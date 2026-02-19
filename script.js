f981a2ce0b3c9cd038902287acb6683d1c7c1949
Shane <robertsjshane@gmail.com>
2026-02-19 13:50:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 import * as GUI from '@babylonjs/gui'
 import { AdvancedDynamicTexture } from '@babylonjs/gui/2D'; 
-
+// Assuming 'scene' is your BABYLON.Scene instance
+const advancedTexture = AdvancedDynamicTexture.CreateFullscreenUI("UI", true, scene);
+// Add controls, for example a button
+// import { Button } from '@babylonjs/gui/2D'; // You would also need to import other controls you use
+// const button = Button.CreateSimpleButton("myButton", "Click Me");
+// advancedTexture.addControl(button);
